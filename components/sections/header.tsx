20e1fb8bc73a6e47edde7f453f3009b9a1441997
NIFFYNATION <adewaoluwanifemi@gmail.com>
2026-08-16 07:14:45 +0100

    logo size

--- components/sections/header.tsx
+++ components/sections/header.tsx
@@ -37,7 +37,7 @@ export function Header() {
             <Image
               src={kesLogo}
               alt={EVENT.name}
-              className="h-10 w-auto transition-opacity duration-300 group-hover:opacity-80 sm:h-12 lg:h-14"
+              className="h-14 w-auto transition-opacity duration-300 group-hover:opacity-80 sm:h-12 lg:h-18"
               preload
             />
           </a>
